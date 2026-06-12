98cb72ff5ecff0af555ee4e044b41267274348ca
Samuel Rodrigues <profsamuelrodrigues@gmail.com>
2026-06-12 17:17:11 -0300

    Criação da página de Login

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,7 +1,43 @@
+import "./SignIn.css";
+import {useState, useEffect} from "react";
+import {Link}from "react-router-dom";
+import logo from "../../assets/logo.png";
+
+
+
 export default function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(()=>{
+        console.log(email)
+        console.log(password)
+    }, [email, password])
+
     return(
-        <div>
-            <h1>Página de Login</h1>
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Logo do Sistema" />
+                </div>
+                <form>
+                    <h1>Entrar</h1>
+                    <input 
+                        type="text" 
+                        placeholder="email@email@gmail.com" 
+                        value={email} 
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="*****" 
+                        value={password} 
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    <button type="submit">Acessar</button>
+                </form>
+                <Link to="/register">Criar Conta</Link>
+            </div>
         </div>
     )
 }
