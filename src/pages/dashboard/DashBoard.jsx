8c4571bf457423ccef44176b3c277704a07ba266
Samuel Rodrigues <profsamuelrodrigues@gmail.com>
2026-06-13 14:58:24 -0300

    Criando a página DashBoard

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -2,6 +2,12 @@ import { useContext } from 'react'
 import {AuthContext} from '../../contexts/auth.jsx'
 
 import Header from '../../components/header/header.jsx'
+import Title from '../../components/title/title.jsx'
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi'
+
+import { Link } from 'react-router-dom'
+
+import './dashboard.css'
 
 export default function Dashboard(){
   const { logout } = useContext(AuthContext);
@@ -12,9 +18,50 @@ export default function Dashboard(){
 
   return(
     <div>
-       <Header/>
-      <h1>Pagina Dashboard</h1>
-      <button onClick={handleLogout}>Sair da conta</button>
+      <Header/>
+
+      <div className="content">
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus color="#FFF" size={25} />
+            Novo chamado
+          </Link>  
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrando em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Mercado Esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">Em Aberto</td>
+                <td data-label="Cadastrado">12/05/2022</td>
+                <td data-label="#">
+                  <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                    <FiSearch color='#FFF' size={17}/>
+                  </button>
+                  <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                    <FiEdit2 color='#FFF' size={17}/>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+
+      </div>
+    
     </div>
   )
 }
